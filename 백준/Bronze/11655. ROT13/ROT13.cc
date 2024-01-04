707bd340e181c642fa38b13d140a9f04d431f8db
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-01-04 12:21:44 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -6,26 +6,13 @@ int main() {
 	getline(cin, str);
 	for (int i = 0; i < str.size(); i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			for (int j = 0; j < 13; j++)
-			{
-				str[i]++;
-				if (str[i] > 'z')
-				{
-					str[i] = str[i] - 'z' + 'a' - 1;
-				}
-			}
+		if (str[i] >= 65 && str[i] < 97) {
+			if (str[i] + 13 > 'Z') str[i] = str[i] + 13 - 'Z' + 'A' - 1;
+			else str[i] += 13;
 		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] += 13;
-			if (str[i] > 'Z')
-			{
-				str[i] -= 'Z';
-				str[i] += 'A';
-				str[i] -= 1;
-			}
+		else if (str[i] >= 97 && str[i] <= 122) {
+			if (str[i] + 13 > 'z') str[i] = str[i] + 13 - 'z' + 'a' - 1;
+			else str[i] += 13;
 		}
 	}
 	cout << str;
