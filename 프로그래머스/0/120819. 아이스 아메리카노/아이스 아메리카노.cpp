b5accde3539e336631c92530634dc6ac9a7d0b72
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-01-24 10:36:52 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 vector<int> solution(int money) {
     vector<int> answer;
-    int cnt = money / 5500;
-    answer.push_back(cnt);
-    answer.push_back(money - (cnt * 5500));
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
     
     return answer;
 }
