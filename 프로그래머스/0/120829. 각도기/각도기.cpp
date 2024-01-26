4772f776cfa00f3e30816aa9177e4d5dc4c91147
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-01-26 13:38:49 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer;
+    if(angle == 180) answer = 4;
+    else if(angle < 180 && angle > 90) answer = 3;
+    else if(angle == 90) answer = 2;
+    else if(angle < 90 && angle > 0) answer = 1;
+    return answer;
+}
