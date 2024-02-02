27a1cad2758992f1ff281ff9bcfbd2faefada374
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-02 12:34:08 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+	int answer = 0;
+	answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+	return answer;
+}
