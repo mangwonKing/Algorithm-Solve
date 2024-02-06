20447d108a99d1d9b42193b23e40980f3b4405a2
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-06 17:24:51 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,80 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
 
-//페어를 편하게 쓰기 위한 매크로
-#define X first
-#define Y second
+int miro[52][52];
+int visited[52][52];
+int n, m, num;
 
-int board[51][51];
-int vis[51][51];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+void dfs(int y , int x)
+{
+	visited[y][x] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m)continue;
+		if (miro[ny][nx] == 0) continue;
+		if (visited[ny][nx] == 1) continue;
+		dfs(ny, nx);
+	}
+}
 
-int main(void) {
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	
+	//getline(cin, str); 공백 포함 입력받기
 	int loop;
 	cin >> loop;
-	while (loop > 0)// 테스트 케이스만큼 돌기
+	while (loop)
 	{
 		
-		int cnt = 0;//흰배추지렁이 마릿수
-		queue<pair<int, int>> q;
-
-		int n, m, k;// 배추밭 길이 와 위치개수
-
-		cin >> n >> m >> k;
-		int x, y; //좌표
-		for (int i = 0; i < 50; i++)
+		int cnt = 0;
+		loop--;
+		cin >> m >> n >> num; 
+		//미로 초기화
+		for (int i = 0; i < 52; i++)
 		{
-			fill(board[i], board[i] + 50, 0);
+			for (int j = 0; j < 52; j++)
+			{
+				miro[i][j] = 0;
+				visited[i][j] = 0;
+			}
 		}
-		for (int i = 0; i < 50; i++)
-		{
 
-			fill(vis[i], vis[i] + 50, -1);// 방문기록 false 로 채우기
-		}
-		for (int i = 0; i < k; i++)//배추밭 채우기
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < num; i++)
 		{
+			
 			cin >> x >> y;
-			board[x][y] = 1;
+			miro[y][x] = 1;
 		}
-		
-		//bfs 시작점 찾기
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < m; j++)
 			{
-				if (board[i][j] == 1 && vis[i][j]==-1)
+				if (miro[i][j] == 1 && visited[i][j] == 0)
 				{
-					q.push({ i,j });
-					vis[i][j] = true;
+					dfs(i,j);
 					cnt++;
-					while (!q.empty())//bfs 돌기
-					{
-						auto cur = q.front(); q.pop();
-						for (int i = 0; i < 4; i++)
-						{
-							int nx = cur.X + dx[i];
-							int ny = cur.Y + dy[i];
-							if (nx < 0 || nx >= 50 || ny < 0 || ny >= 50)continue;
-							if (vis[nx][ny] >= 0 || board[nx][ny] == 0)continue;
-							q.push({ nx,ny });
-							vis[nx][ny] = 0;
-						}
-					}
 				}
 			}
 		}
-		cout << cnt << "\n";
-		loop--;
+		cout << cnt<<"\n";
 	}
-	
-
-	
-
-	
+	return 0;
 }
-
