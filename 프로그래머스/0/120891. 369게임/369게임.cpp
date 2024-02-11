6a288fec97bfacc4199bc73a26fb95ba7b556c7e
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-11 15:23:27 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    while(order > 0)
+    {
+        if(order % 10 == 3 ||order % 10 == 6 ||order % 10 == 9 )
+        {
+            answer++;
+        }
+        order /= 10;
+    }
+    return answer;
+}
