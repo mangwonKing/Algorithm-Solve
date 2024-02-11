2571004604ba2ab34799abc9fc00e36fc11189bc
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-11 15:26:32 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    for(int i = 0 ; i < cipher.size(); i++)
+    {
+        if(i % code == (code-1) )
+        {
+            answer.push_back(cipher[i]);
+        }
+    }
+    return answer;
+}
