10cb018ddff31a842e8451a2115060b73df19a6b
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-24 11:38:48 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2444 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,63 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int paper[102][102];
+int vis[102][102];
 int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
-int paper[102][102] = { 0 };
-int visited[102][102] = { 0 };
 
-int n, m, k;
+vector<int> area;
+int m, n, k;
 
-int dfs(int y, int x)
+int dfs(int y , int x)
 {
-	visited[y][x] = 1;
-	int area = 1;
+	vis[y][x] = 1;
+	int ret = 1;
 	for (int i = 0; i < 4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny < 0 || ny >= m || nx < 0 || nx >= n)continue;
-		if (paper[ny][nx] == 1 || visited[ny][nx] == 1)continue;
-		area += dfs(ny, nx);
+		if (ny < 0 || nx < 0 || ny >= m || nx >= n)continue;
+		if (vis[ny][nx] == 1 || paper[ny][nx] == 1)continue;
+		ret += dfs(ny, nx);
 	}
-	return area;
+	return ret;
 }
-
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 	//getline(cin, str); 공백 포함 입력받기
 	cin >> m >> n >> k;
-	vector<int>areas;
-	int cnt = 0;
-	for (int i = 0; i < k; i++)
+	for (int i = 0;i < k; i++)
 	{
-		int ax, ay, bx, by;
-		cin >> ax >> ay >> bx >> by;
-		for (int y = ay; y < by; y++)
-		{
-			for (int x = ax; x < bx; x++)
-			{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		for (int y = y1; y < y2; y++)
+			for (int x = x1; x < x2; x++)
 				paper[y][x] = 1;
-			}
-		}
 	}
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (paper[i][j] == 0 && visited[i][j] == 0)
+			if (paper[i][j] == 0 && vis[i][j] == 0)
 			{
-				areas.push_back(dfs(i,j));
-				cnt++;
+				area.push_back(dfs(i, j));
 			}
 		}
 	}
-	sort(areas.begin(), areas.end());
-	cout << cnt << "\n";
-	for (auto a : areas)
+	sort(area.begin(), area.end());
+	cout << area.size() << "\n";
+	for (auto a : area)
 	{
 		cout << a << " ";
 	}
+	
 	return 0;
 }
