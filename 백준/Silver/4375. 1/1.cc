b3e94195f5f4a06635f019fb0f5318747abd4483
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-25 14:23:35 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 	//getline(cin, str); 공백 포함 입력받기
 	
 	int n;
 	while (cin >> n)
 	{
-		
-		ll i = 10;
-		ll a = 1;
 		int cnt = 1;
-		while (true)
+		int i = 1;
+		while (i % n != 0)
 		{
-			if (a % n == 0)break;
-			a = (a * 10 + 1);
-			a %= n;
+			i = (i * 10 + 1) % n;
 			cnt++;
 		}
-		cout << cnt<<"\n";
+		cout << cnt << "\n";
 	}
 	
 	return 0;
-	
 }
