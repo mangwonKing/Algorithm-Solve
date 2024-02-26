53fd9a7b4a4996a95873264934bced325dd7469d
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-02-26 12:55:09 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string s = to_string(n);
+    for(int i = 0; i < s.size(); i++)
+        answer += s[i] - '0';
+    return answer;
+}
