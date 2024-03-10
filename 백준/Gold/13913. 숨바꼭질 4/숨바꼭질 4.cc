625aaa9c78a8d59a6cab18caed9f48493401423b
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-03-10 18:35:44 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 88 ms, Memory: 12708 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int vis[200002];
+int subin;
+int sis;
+int mn;
+map<int, int> re;
+stack<int> way;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	//getline(cin, str); 공백 포함 입력받기
+	cin >> subin >> sis;
+	if (subin == sis)
+	{
+		cout << 0 << "\n";
+		cout << sis;
+		return 0;
+	}
+	vis[subin] = 1;
+	queue<int> q;
+	q.push(subin);
+	re[subin] = -1;
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		for (int next : {x - 1, x + 1, x * 2})
+		{
+			if (next >= 0 && next <= 200000)
+			{
+				if (!vis[next])
+				{
+					q.push(next);
+					if (!re[next]) re[next] = x;
+					vis[next] = vis[x] + 1;
+				}
+			}
+		}
+	}
+	cout << vis[sis]-1 << "\n";
+	way.push(sis);
+	int k = re[sis];
+	while(way.size() < vis[sis])
+	{
+		way.push(k);
+		k = re[k];
+	}
+	while (!way.empty())
+	{
+		cout << way.top() << " ";
+		way.pop();
+	}
+	return 0;
+}
