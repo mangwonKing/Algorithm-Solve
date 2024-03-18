5dcb56652f0e38d7fd227985979620617e956055
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-03-18 21:18:54 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nums[2050];
+int tree[2050];
+int k;
+
+int cnt = 0;
+void preOrder(int now)
+{
+	if (tree[now*2] == 0) //tree의 마지막 이라면
+	{
+		tree[now] = nums[++cnt];
+		return;
+	}
+	preOrder(now * 2);
+	tree[now] = nums[++cnt];
+	preOrder(now * 2 + 1);
+
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	//getline(cin, str); 공백 포함 입력받기
+	cin >> k;
+	for (int i = 1; i < pow(2, k); i++)
+	{
+		cin >> nums[i];
+		tree[i] = 1;
+	}
+	preOrder(1);
+	for (int i = 1; i <= k; i++)
+	{
+		for (int j = pow(2,i-1); j < pow(2, i); j++)
+		{
+			cout << tree[j] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
