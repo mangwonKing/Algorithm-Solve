21ad2bc0f880697f10da93d4a2b0303659b813fa
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-08 15:28:55 +0900

    Update 치킨 배달.cc
    5/8 15686 1회차 다시풀기 성공 , 30분

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,62 +1,66 @@
+// 5/8 다시 푼것
 #include <bits/stdc++.h>
 using namespace std;
 
+int city[52][52];
+int minc = INT_MAX;
+
+vector<pair<int, int>> home;
+vector<pair<int, int>> chicken;
+vector<pair<int, int>> Select;
 
 int n, m;
-int city[52][52];
-vector < pair<int, int>> chiks;
-vector < pair<int, int>> homes;
-vector <vector<int>> chlist;
-void combi(int start , vector<int> v)
+
+int calDist(pair<int, int> a, pair <int, int> b)
 {
-	if (v.size() == m)
-	{
-		chlist.push_back(v);
-		return;
-	}
-	for (int i = start + 1; i < chiks.size(); i++)
-	{
-		v.push_back(i);
-		combi(i, v);
-		v.pop_back();
-	}
+    return abs(a.first - b.first) + abs(a.second - b.second);
 }
-int dist = INT_MAX;
+
+void go(int now)
+{
+    if (Select.size() == m)
+    {
+        int total = 0;
+        for (auto h : home)
+        {
+            int tmp = 987654321;
+            for (auto s : Select)
+            {
+                int dist = calDist(h, s);
+                if (tmp > dist)tmp = dist; //각 집별로 최소인 치킨거리 더해주기
+            }
+            total += tmp;
+        }
+        if (minc > total) minc = total;
+        return;
+    }
+
+    for (int i = now; i < chicken.size(); i++)
+    {
+        Select.push_back(chicken[i]);
+        go(i + 1);
+        Select.pop_back();
+        go(i + 1);
+    }
+    return;
+}
+
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	//getline(cin, str); 공백 포함 입력받기
-	
-	//입력
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cin >> city[i][j];
-			if (city[i][j] == 1)homes.push_back({ i,j });
-			else if (city[i][j] == 2)chiks.push_back({ i,j });
-		}
-	}
-
-	// 가능한 치킨집의 조합을 구하기
-	vector<int> v;
-	combi(-1, v);
-	// 조합을 하나씩 꺼내서 각 집별 가까운 치킨집의 거리 저장하고 최소값의 조합 찾기
-
-	for (vector<int> ch : chlist)
-	{
-		int res = 0;
-		for (pair<int, int> home : homes)
-		{	
-			int _min = INT_MAX;
-			for (int idx : ch)
-				_min = min(_min, abs(home.first - chiks[idx].first) + abs(home.second - chiks[idx].second));
-			res += _min;
-		}
-		dist = min(dist, res);
-	}
-	cout << dist;
-	
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //getline(cin, str); 공백 포함 입력받기
+    
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> city[i][j];
+            if (city[i][j] == 1)home.push_back({ i,j });
+            else if (city[i][j] == 2)chicken.push_back({ i,j });
+        }
+    }
+    go(0);
+    cout << minc;
+    return 0;
 }
