3b73ff782c1b002a39dc41f220315be1f916cd73
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-09 12:06:58 +0900

    Update 빈도 정렬.cc
    5/9 다시풀기

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,44 +1,46 @@
+// 5/9 1회차 다시풀기 성공
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-map<ll,ll> mp;
-map<ll, ll> mp2;
-vector <pair<ll, ll>> v;
 int n, c;
-
-bool cmp(pair<ll, ll> a, pair<ll, ll>b)
+map<int, int> code;
+map<int, int> pass;
+vector<pair<int, int>> code2;
+bool cmp(pair<int, int> a, pair<int, int> b)
 {
-	if (a.second == b.second)
-		return mp2[a.first] < mp2[b.first];
-	return a.second > b.second;
+    if (a.second == b.second)
+    {
+        return pass[a.first] < pass[b.first];
+    }
+    return a.second > b.second;
 }
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	//getline(cin, str); 공백 포함 입력받기
-	cin >> n >> c;
-	for (int i = 0; i < n; i++)
-	{
-		ll a;
-		cin >> a;
-		mp[a]++;
-		if (mp2[a] == 0)
-			mp2[a] = i+1;
-	}
-	for (auto a : mp)
-	{
-		v.push_back(a);
-	}
-	sort(v.begin(), v.end(), cmp);
-	for (auto a : v)
-	{
-		for (int i = 0; i < a.second; i++)
-		{
-			cout << a.first << " ";
-		}
-		
-	}
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //getline(cin, str); 공백 포함 입력받기
+    cin >> n >> c;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        code[a]++;
+        if (pass[a] == 0) 
+        {
+            if (i == 0)pass[a] = 1;
+            else pass[a] = i+1; 
+        }
+    }
+    for (auto a : code)
+    {
+        code2.push_back({ a.first,a.second });
+    }
+    sort(code2.begin(), code2.end(), cmp);
+    for (auto a : code2)
+    {
+        for (int i = 0; i < a.second; i++)
+        {
+            cout << a.first << " ";
+        }
+    }
+    return 0;
 }
