845b2a14839a90c86bca057166dff45ef5e5966e
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-13 11:47:38 +0900

    Update 영역 구하기.cc
    5/13 다시풀이 성공

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,57 +1,64 @@
+// 5-13 다시풀이 20분컷 
+// 아쉬운점 : total 을 구할 필요 없었음 그냥 area의 size 가 총 영역의 개수임
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m, k;
+int total = 0;
 int paper[102][102];
 int vis[102][102];
-int dy[4] = { -1,0,1,0 };
-int dx[4] = { 0,1,0,-1 };
-
+int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1,0,-1,0 };
 vector<int> area;
-int m, n, k;
 
-int dfs(int y , int x)
+int dfs(int a,int b)
 {
-	vis[y][x] = 1;
-	int ret = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= m || nx >= n)continue;
-		if (vis[ny][nx] == 1 || paper[ny][nx] == 1)continue;
-		ret += dfs(ny, nx);
-	}
-	return ret;
+    vis[a][b] = 1;
+    int ret = 1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = a + dy[i];
+        int nx = b + dx[i];
+        if (ny >= n || ny < 0 || nx >= m || nx < 0)continue;
+        if (vis[ny][nx] != 0 || paper[ny][nx] == 1) continue;
+        ret += dfs(ny, nx);
+    }
+    return ret;
 }
+
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	//getline(cin, str); 공백 포함 입력받기
-	cin >> m >> n >> k;
-	for (int i = 0;i < k; i++)
-	{
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		for (int y = y1; y < y2; y++)
-			for (int x = x1; x < x2; x++)
-				paper[y][x] = 1;
-	}
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (paper[i][j] == 0 && vis[i][j] == 0)
-			{
-				area.push_back(dfs(i, j));
-			}
-		}
-	}
-	sort(area.begin(), area.end());
-	cout << area.size() << "\n";
-	for (auto a : area)
-	{
-		cout << a << " ";
-	}
-	
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //getline(cin, str); 공백 포함 입력받기
+    cin >> n >> m >> k; 
+    for (int i = 0; i < k; i++)
+    { // x , y
+        int x1, x2, y1, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for (int j = y1; j < y2; j++)
+        {
+            for (int k = x1; k < x2; k++)
+            {
+                paper[j][k] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (paper[i][j] == 0 && vis[i][j] == 0)
+            {
+                total++;
+                area.push_back(dfs(i,j));
+            }
+        }
+    }
+    sort(area.begin(), area.end());
+    cout << total<<"\n";
+    for (auto a : area)
+        cout << a << " ";
+    return 0;
 }
