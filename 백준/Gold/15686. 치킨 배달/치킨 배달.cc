c865d98ba1f5186f93b1ce583a4ff3a26fe64731
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-15 20:20:03 +0900

    Update 치킨 배달.cc
    5/15 2회차 다시풀기 18분

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,66 +1,62 @@
-// 5/8 다시 푼것
+//5/15 다시풀기 성공
 #include <bits/stdc++.h>
 using namespace std;
 
 int city[52][52];
-int minc = INT_MAX;
-
-vector<pair<int, int>> home;
 vector<pair<int, int>> chicken;
-vector<pair<int, int>> Select;
-
+vector<pair<int, int>> home;
+vector<pair<int, int>> chlist;
 int n, m;
+int mn = 987654321;
 
-int calDist(pair<int, int> a, pair <int, int> b)
+int dist(pair<int, int> a, pair<int, int>b)
 {
     return abs(a.first - b.first) + abs(a.second - b.second);
 }
-
-void go(int now)
+void choice(int now)
 {
-    if (Select.size() == m)
+    if (chlist.size() == m)
     {
         int total = 0;
         for (auto h : home)
         {
-            int tmp = 987654321;
-            for (auto s : Select)
+            int m = 987654321;
+            for (auto ch : chlist)
             {
-                int dist = calDist(h, s);
-                if (tmp > dist)tmp = dist; //각 집별로 최소인 치킨거리 더해주기
+                int temp = dist(ch, h);
+                if (m > temp) m = temp;
             }
-            total += tmp;
+            total += m;
         }
-        if (minc > total) minc = total;
+        if (mn > total) mn = total;
         return;
     }
 
     for (int i = now; i < chicken.size(); i++)
     {
-        Select.push_back(chicken[i]);
-        go(i + 1);
-        Select.pop_back();
-        go(i + 1);
+        chlist.push_back({ chicken[i].first ,chicken[i].second });
+        choice(i + 1);
+        chlist.pop_back();
+        choice(i + 1);
     }
-    return;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     //getline(cin, str); 공백 포함 입력받기
-    
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> city[i][j];
             if (city[i][j] == 1)home.push_back({ i,j });
-            else if (city[i][j] == 2)chicken.push_back({ i,j });
+            else if (city[i][j] == 2) chicken.push_back({ i,j });
         }
     }
-    go(0);
-    cout << minc;
+    choice(0);
+    cout << mn;
     return 0;
 }
