1c54f9d12ee4029bb170316d387008fff3ee0909
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-16 22:58:46 +0900

    Update 빈도 정렬.cc 2회차 다시풀기

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,46 +1,36 @@
-// 5/9 1회차 다시풀기 성공
+// 5-16 2회차 7일 후 다시풀기 완벽 성공 (15분컷, 코드 더 간결) 
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, c;
-map<int, int> code;
-map<int, int> pass;
-vector<pair<int, int>> code2;
+map<int, int> msg;
+map<int, int> idx; // 빈도가 같을경우 등장한 순서를 기록하기 위한 map
+vector<pair<int, int>> deco; // 커스텀 정렬을 위해 map 저장할 그릇
+
 bool cmp(pair<int, int> a, pair<int, int> b)
 {
-    if (a.second == b.second)
-    {
-        return pass[a.first] < pass[b.first];
-    }
-    return a.second > b.second;
+	if (a.second == b.second)return idx[a.first] < idx[b.first];
+	return a.second > b.second;
 }
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    //getline(cin, str); 공백 포함 입력받기
-    cin >> n >> c;
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        cin >> a;
-        code[a]++;
-        if (pass[a] == 0) 
-        {
-            if (i == 0)pass[a] = 1;
-            else pass[a] = i+1; 
-        }
-    }
-    for (auto a : code)
-    {
-        code2.push_back({ a.first,a.second });
-    }
-    sort(code2.begin(), code2.end(), cmp);
-    for (auto a : code2)
-    {
-        for (int i = 0; i < a.second; i++)
-        {
-            cout << a.first << " ";
-        }
-    }
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	//getline(cin, str); 공백 포함 입력받기
+	cin >> n >> c;
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		msg[a]++;
+		if (idx[a] == 0)
+			idx[a] = i + 1;
+	}
+	for (auto a : msg)
+		deco.push_back(a);
+	sort(deco.begin(), deco.end(), cmp);
+	for (auto a : deco)
+		for (int i = 0; i < a.second; i++)
+			cout << a.first << " ";
+
+	return 0;
 }
