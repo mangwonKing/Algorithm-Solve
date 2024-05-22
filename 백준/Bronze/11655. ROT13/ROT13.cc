b014c6397063b4e5b2f62ff28dc43a2755f79bb6
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-22 15:55:39 +0900

    Update ROT13.cc 5/22 2회차 다시풀기 성공
    20분

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,31 +1,32 @@
+// 5/22 2회차 다시풀기 20분 성공
 #include <bits/stdc++.h>
 using namespace std;
 
+string str;
+string str2;
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    //getline(cin, str); 공백 포함 입력받기
-    string str;
-    string code;
-    getline(cin, str);
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	//getline(cin, str); 공백 포함 입력받기 
 
-    for (auto s : str)
-    {
-        if (s >= 'a' && s <= 'z')
-        {
-            if (s + 13 > 'z')code.push_back(s + 13 - 'z' + 'a'-1);
-            else code.push_back(s + 13);
-        }
-        else if (s >= 'A' && s <= 'Z')
-        {
-            if (s + 13 > 'Z')code.push_back(s + 13 - 'Z' + 'A'-1);
-            else code.push_back(s + 13);
-        }
-        else
-        {
-            code.push_back(s);
-        }
-    }
-    cout << code;
-    return 0;
+	getline(cin, str);
+	for (auto ch : str)
+	{
+		
+		if (ch >= 'a' && ch <= 'z')
+		{
+			if (ch + 13 > 'z')ch = ch + 13 - 'z' + 'a'-1;
+			else ch = ch + 13;
+			str2.push_back(ch);
+		}
+		else if (ch >= 'A' && ch <= 'Z')
+		{
+			if (ch + 13 > 'Z')ch = ch + 13 - 'Z' + 'A'-1;
+			else ch = ch + 13;
+			str2.push_back(ch);
+		}
+		else str2.push_back(ch);
+	}
+	cout << str2;
+	return 0;
 }
