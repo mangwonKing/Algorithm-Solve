fcb10c512b2aff3f760b5739c3f3644af6790c8d
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-22 15:17:17 +0900

    Update 1.cc 5-22 다시풀기 1회차

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,23 +1,25 @@
+// 5/22 1회차 다시풀기 성공 20분
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	//getline(cin, str); 공백 포함 입력받기
-	
-	int n;
-	while (cin >> n)
+	while (cin >> n)//입력이 끝나는 조건이 없을 때 입력받기
 	{
+		int idx = 1;
 		int cnt = 1;
-		int i = 1;
-		while (i % n != 0)
+		while (true) 
 		{
-			i = (i * 10 + 1) % n;
+			idx = idx % n;
+			if (idx == 0)break;
 			cnt++;
+			idx = idx *10 +1; // 자릿수 늘려가기
 		}
-		cout << cnt << "\n";
+		cout << cnt<<"\n";
 	}
-	
 	return 0;
 }
