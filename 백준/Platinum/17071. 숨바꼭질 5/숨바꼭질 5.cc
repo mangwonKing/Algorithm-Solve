69353dbd46cf06711b592ddf05e5f5f51f8be344
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-23 12:27:41 +0900

    Update 숨바꼭질 5.cc 5/23 1회차 다시풀기 성공
    기억말고 생각!

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,53 +1,51 @@
-// 5/20 다시풀기 실패 시간주기관리 및 bfs를 활용한 flood fill 떠올리기, 3일 뒤 다시하기
+// 5/23 다시풀기 성공 , 기억하지 말고 생각해서 풀어보기 , 30분컷
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int sb, sis;
-int vis[2][500002];// 홀 수 짝 수
-queue<int> q;
+int n, k;
+int vis[2][500002]; // 짝수,홀수 차례에 방문한 길 기록
 int sec = 0;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	//getline(cin, str); 공백 포함 입력받기
-	cin >> sb>> sis;
-	if (sb == sis)
+	//getline(cin, str); 공백 포함 입력받기 
+	cin >> n >> k;
+	if (n == k)
 	{
 		cout << 0;
 		return 0;
 	}
-	vis[sec][sb] = 1; //수빈이 이동
-	q.push(sb);
-	while (q.size()) 
+	queue<int> q;
+	vis[0][n] = 1;// 수빈이 시작시점
+	q.push(n); // 시작지점 q에 푸쉬
+	while (1)
 	{
-		int size = q.size();
+		//1초 = 1레벨
 		sec++;
-		sis += sec; //동생과 같은 레벨로 실행
-		if (sis > 500000)
-			break;
-		for (int i = 0; i < size; i++)
+		k += sec; //수빈이 위치 초만큼 +하기 
+		if (k > 500000) break;
+		int size = q.size();
+		while (size--)// 한 레벨씩 진행
 		{
-			int x = q.front();
+			int now = q.front();
 			q.pop();
-
-			for (int next : {x - 1, x + 1, x * 2})
+			for (int next : {now + 1, now - 1, now * 2})
 			{
-				if (next <= 500000 && next >= 0)
+				if (next > 500000 || next < 0) continue; // 0보다 작거나 50만보다 클 수 없다. 이걸 먼저 조사해야 out of bounds 를 피할 수 있다.
+				if (vis[sec % 2][next])continue;//같은 홀 or 짝 에 방문한 적 있으면 continue
+				q.push(next);
+				vis[sec % 2][next] = 1;
+				if (vis[sec % 2][k])// 동생이 방문할 지점을 같은 홀 or 짝에 수빈이가 방문을 이미 했다면 => 만날 수 있다.
 				{
-					if (vis[sec % 2][next])continue; //짝수 , 홀수회차에 방문한 적있으면 pass 
-					vis[sec & 2][next] = 1;
-					q.push(next);
-					if (vis[sec % 2][sis]) //짝수,홀수 회차에 동생이 해당지점을 방문할 수 있다면
-					{
-						cout << sec; //그것이 최소거리 (최단거리)
-						return 0;
-					}
+					cout << sec;
+					return 0;
 				}
 			}
 		}
 	}
-	cout << -1;
+	cout << -1;// 찾지 못하고 while 을 나온 경우
+	
 	return 0;
 }
