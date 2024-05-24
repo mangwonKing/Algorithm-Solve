7f5a4f0c67361b1eea70bc2957085a4bb8c7802c
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-24 12:55:00 +0900

    Update 곱셈.cc 5/24 1회차 다시풀기 성공
    생각을 하자

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,28 +1,25 @@
+//5/24 다시풀기 성공 , 외우지말고 생각해서 풀기
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 
-using ll = long long;
+ll a, b, c;
 
-ll Pow(ll a, ll b, ll c)
+ll go(ll now)//(a * a) % c  = ((a%c)*(a%c))%c 임을 이용
 {
-	if (b == 1) return a % c;
-	ll val = Pow(a, b/2, c);
-	val = val * val % c;
-	if (b % 2 == 0) return val;
-	return val * a % c;
+	if (now == 1)
+		return a % c;
+	if (now % 2 == 0)return go(now / 2) * go(now / 2) % c; // 짝수승
+	else return go(now-1) * go(1) % c; //홀수승은 짝수로 만들어주기
 }
 
-
-int main(void) {
+int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	ll a, b, c;
+	cin.tie(0); cout.tie(0);
+	//getline(cin, str); 공백 포함 입력받기 
 	cin >> a >> b >> c;
-	cout << Pow(a, b, c);
-	
+
+	cout << go(b);
 	return 0;
-
 }
-
