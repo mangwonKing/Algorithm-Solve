32a2cd6f1d7d955e9957d6dbf616e2324ab7a09b
mangwonKing <91066935+mangwonKing@users.noreply.github.com>
2024-05-28 23:31:27 +0900

    [level 0] Title: 문자열 돌리기, Time: 1.96 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(auto a : str)
+    {
+        cout<<a<<endl;
+    }
+    return 0;
+}
